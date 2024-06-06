a2891e608cf50004c615b5c671223c8bf795b20c
Djoseffer <tom_djosefer@hotmail.com>
2024-06-05 21:05:01 -0300

    feat: new methods https

--- src/main/java/com/apprh/controllers/VagaController.java
+++ src/main/java/com/apprh/controllers/VagaController.java
@@ -1,23 +1,50 @@
 package com.apprh.controllers;
 
-import com.apprh.repositories.CandidatoRepository;
-import com.apprh.repositories.VagaRepository;
+import com.apprh.domain.models.Vaga;
+import com.apprh.dtos.VagaDTO;
+import com.apprh.services.CandidatoService;
+import com.apprh.services.VagaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/cadastrar")
 public class VagaController {
 
     @Autowired
-    private VagaRepository vgRepository;
+    private VagaService vagaService;
     @Autowired
-    private CandidatoRepository cdRepository;
+    private CandidatoService candidatoService;
 
     @GetMapping
     public String form (){
         return "vaga/formVaga";
+    }
+
+    @PostMapping
+    public ResponseEntity<Vaga> form(@Valid VagaDTO vagaDTO){
+        var newRegister = new Vaga();
+        BeanUtils.copyProperties(vagaDTO, newRegister);
+        vagaService.save(newRegister);
+        return ResponseEntity.status(HttpStatus.OK).body(newRegister);
+    }
+
+    @GetMapping("/vagas")
+    public ResponseEntity<List<Vaga>> listaVagas(){
+        var vagas = vagaService.getAll();
+        return ResponseEntity.status(HttpStatus.OK).body(vagas);
+    }
+
+    @GetMapping("/vagas/{id}")
+    public ResponseEntity<Object> detalhesVaga(@PathVariable Long id){
+        Optional<Vaga> buscaVaga = vagaService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(buscaVaga.get());
     }
 }
